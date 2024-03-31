2ef13f058b0b61ca4b4dddd41091e39cc1d5cab9
Yashwanth sai <20r91a0530@tkrec.ac.in>
2024-03-31 16:30:46 +0530

    added postgre deployed database url and updated sequilze

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -2,9 +2,13 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize('pixelspeak', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
+    native: true
+  }
 });
 
 export default sequelize;
