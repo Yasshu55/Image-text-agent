93e5b7eaf4ab9f1cd13e355d49d3f1e06624e186
Yashwanth sai <20r91a0530@tkrec.ac.in>
2024-03-31 16:34:31 +0530

    SSL self signed issue

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -6,7 +6,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false, // This line is added to bypass the self-signed certificate issue
+    },
     native: true
   }
 });
